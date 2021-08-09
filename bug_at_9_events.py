3fc100ae3a8da24074726977f09e4ba68317feae
mmansanelli <76078354+mmansanelli@users.noreply.github.com>
2021-08-09 20:30:53 -0300

    Update bug_at_9_events.py

--- bug_at_9_events.py
+++ bug_at_9_events.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  9 15:08:55 2021
-
-@author: Marina ♡
-"""
 from __future__ import absolute_import
 import networkx as nx
 import numpy as np
+from hypergraphs import Hypergraph
+from directed_structures import DirectedStructure
 from mDAG_advanced import mDAG
 
 
-G_problem=mDAG(directed_structure((0,1,2), [(1,0),(1,2)]),hypergraph([(0,1),(1,2)]))
+G_problem=mDAG(DirectedStructure( [(1,0),(1,2)],3),Hypergraph([(0,1),(1,2)],3))
 
 infeasible = G_problem.smart_infeasible_binary_supports_n_events(9)
 
