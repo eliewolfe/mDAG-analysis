9122ffbd79b0099217dd09f32bb4df2d8f85aa78
Elie Wolfe <elupus@gmail.com>
2021-08-09 14:48:27 -0400

    Adding a function to identify mDAGs with no infeasible supports up to n_events.

--- mDAG_advanced.py
+++ mDAG_advanced.py
@@ -196,7 +196,7 @@ class mDAG:
 
     def no_infeasible_supports_up_to(self, max_n, **kwargs):
         return all(self.smart_support_testing_instance(n).unique_infeasible_supports_unlabelled(**kwargs, name='mgh', use_timer=False) for
-                   n in range(2,n+1))
+                   n in range(2,max_n+1))
 
 
 
