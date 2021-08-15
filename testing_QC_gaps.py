93ed36dc8e9b4c0c4b34857c33c29b22ddff2d3a
Marina Maciel Ansanelli <ansanelli.marina@gmail.com>
2021-08-15 14:40:44 -0300

    In metagraph_advanced.py, found a list of the graph patterns that don't have any infeasible supports beyond e-sep (45 graph patterns when searching up to 5 events). Created the file testing_QC_gaps.py that tests the existence of infeasible supports beyond e-sep for some examples (Bell, Triangle, Square, Instrumental, Evans). Found that even at cardinality 3, where the Instrumental Scenario has a QC gap, it does not present any infeasible supports beyond e-sep up to 10 events.

---
+++ testing_QC_gaps.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import
+import networkx as nx
+import numpy as np
+from hypergraphs import Hypergraph
+from directed_structures import DirectedStructure
+from mDAG_advanced import mDAG
+
+
+G_Bell=mDAG(DirectedStructure([(0,3),(1,2)],4),Hypergraph([(0,),(1,),(2,3)],4))
+
+for i in range(2,5):
+    infeasible = G_Bell.smart_infeasible_binary_supports_n_events(i)
+    print(infeasible)
+    
+for c in Observable_mDAGs.foundational_eqclasses:
+    if G_Bell in c:
+        for element in c:
+            print(element in no_inf_sups_beyond_esep)
+            
+#########################          
+            
+G_Triangle=mDAG(DirectedStructure( [],3),Hypergraph([(0,1),(1,2),(2,0)],3))
+
+for i in range(2,9):
+    infeasible = G_Triangle.smart_infeasible_binary_supports_n_events(i)
+    print(infeasible)
+    
+for c in Observable_mDAGs3.foundational_eqclasses:
+    if G_Triangle in c:
+        for element in c:
+            print(element in no_inf_sups_beyond_esep3)
+        
+######################### 
+
+G_Square=mDAG(DirectedStructure( [],4),Hypergraph([(0,1),(1,2),(2,3),(3,0)],4))
+    
+for c in Observable_mDAGs.foundational_eqclasses:
+    if G_Bell in c:
+        for element in c:
+            print(element in no_inf_sups_beyond_esep)
+            
+######################### 
+
+G_Instrumental=mDAG(DirectedStructure( [(0,1),(1,2)],3),Hypergraph([(0,),(1,2)],3))
+
+for i in range(2,9):
+    infeasible = G_Instrumental.smart_infeasible_binary_supports_n_events(i)
+    print(infeasible)
+    
+for c in Observable_mDAGs3.foundational_eqclasses:
+    if G_Instrumental in c:
+        for element in c:
+            print(element in no_inf_sups_beyond_esep3)
+
+for i in range(2,11):
+    infeasible3=G_problem.smart_infeasible_supports_n_events_card_3(i)
+    print(infeasible3)
+
+######################### 
+    
+G_Evans=mDAG(DirectedStructure( [(0,1),(0,2)],3),Hypergraph([(0,1),(0,2)],3))
+
+for i in range(2,9):
+    infeasible = G_Evans.smart_infeasible_binary_supports_n_events(i)
+    print(infeasible)
+
+for c in Observable_mDAGs3.foundational_eqclasses:
+    if G_Evans in c:
+        for element in c:
+            print(element in no_inf_sups_beyond_esep3)
+
+for i in range(2,9):
+    infeasible3=G_Evans.smart_infeasible_supports_n_events_card_3(i)
+    print(infeasible3)
+    
+
+
+
+    
