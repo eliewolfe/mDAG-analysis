Major bugfix: Previous calculation of "unlabelled infeasible supports" was erroneous.

Older versions/mDAG.py
@@ -178,7 +178,7 @@ class mDAG:
     def infeasible_binary_supports_n_events_unlabelled(self, n):
         return SupportTesting(self.parents_of_for_supports_analysis,
                               np.broadcast_to(2, self.number_of_visible),
-                              n).unique_infeasible_supports_unlabelled(name='mgh', use_timer=False)
+                              n).unique_infeasible_supports_as_integers_unlabelled(name='mgh', use_timer=False)
 
     def smart_infeasible_binary_supports_n_events_unlabelled(self, n):
         return SmartSupportTesting(self.parents_of_for_supports_analysis,