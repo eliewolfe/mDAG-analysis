3af8c491526f4ff70ad231231b5a84f5ac0b2392
Marina <ansanelli.marina@gmail.com>
2022-03-21 16:23:59 -0400

    Bug Fix: Included two other versions of the instrumental scenario to the list of known QC Gaps. Now, got a number of 211 QC Gaps out of the 331 fundamental classes by fixing to a point distribution. This bug fix showed that we can in fact look only at representatives of the eq classes; this gave the same result as checking every mDAG.

--- proving_QC_Gaps.py
+++ proving_QC_Gaps.py
@@ -11,15 +11,19 @@ if __name__ == '__main__':
     #Observable_mDAGs3 = Observable_mDAGs_Analysis(nof_observed_variables=3, max_nof_events_for_supports=0)
     Observable_mDAGs4 = Observable_mDAGs_Analysis(nof_observed_variables=4, max_nof_events_for_supports=0)
     
-    G_Instrumental=mDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([(1,2)],3))
+    G_Instrumental1=mDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([(1,2)],3))
+    G_Instrumental2=mDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([(0,1),(1,2)],3))
+    G_Instrumental3=mDAG(DirectedStructure([(1,2)],3),Hypergraph([(0,1),(1,2)],3))
     G_Bell= mDAG(DirectedStructure([(0,1),(2,3)],4),Hypergraph([(1,2)],4))
     G_Triangle=mDAG(DirectedStructure([],3),Hypergraph([(1,2),(2,0),(0,1)],3))
     G_Evans=mDAG(DirectedStructure([(0,1),(0,2)],3),Hypergraph([(0,1),(0,2)],3))
-    known_QC_Gaps_mDAGs_id=[G_Instrumental.unique_unlabelled_id,G_Bell.unique_unlabelled_id,G_Triangle.unique_unlabelled_id]
+    known_QC_Gaps_mDAGs_id=[G_Instrumental1.unique_unlabelled_id,G_Instrumental2.unique_unlabelled_id,G_Instrumental3.unique_unlabelled_id,G_Bell.unique_unlabelled_id,G_Triangle.unique_unlabelled_id]
     #known_QC_Gaps_mDAGs_id=[G_Evans.unique_unlabelled_id,G_Instrumental.unique_unlabelled_id,G_Bell.unique_unlabelled_id,G_Triangle.unique_unlabelled_id]
 
     
-    QG_Instrumental=QmDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([],3),Hypergraph([(1,2)],3))
+    QG_Instrumental1=QmDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([],3),Hypergraph([(1,2)],3))
+    QG_Instrumental2=QmDAG(DirectedStructure([(0,1),(1,2)],3),Hypergraph([],3),Hypergraph([(0,1),(1,2)],3))
+    QG_Instrumental3=QmDAG(DirectedStructure([(1,2)],3),Hypergraph([],3),Hypergraph([(0,1),(1,2)],3))
     QG_Bell= QmDAG(DirectedStructure([(0,1),(2,3)],4),Hypergraph([],4),Hypergraph([(1,2)],4))
     known_QC_Gaps_QmDAGs_id=[QG_Instrumental.unique_unlabelled_id,QG_Bell.unique_unlabelled_id]
 
@@ -30,14 +34,20 @@ if __name__ == '__main__':
                     return True
         return False
     
+# =============================================================================
+#     QC_gap_by_Intervention=[]
+#     for eqclass in Observable_mDAGs4.foundational_eqclasses:
+#         smart_representative=Observable_mDAGs4.representative_mDAGs_list[Observable_mDAGs4.foundational_eqclasses.index(eqclass)]
+#         for mDAG in eqclass:
+#             if reduces_to_knownQCGap_by_intervention(mDAG):
+#                 QC_gap_by_Intervention.append(smart_representative)
+#                 break
+# =============================================================================
+
     QC_gap_by_Intervention=[]
-    for eqclass in Observable_mDAGs4.foundational_eqclasses:
-        smart_representative=Observable_mDAGs4.representative_mDAGs_list[Observable_mDAGs4.foundational_eqclasses.index(eqclass)]
-        for mDAG in eqclass:
-            if reduces_to_knownQCGap_by_intervention(mDAG):
-                QC_gap_by_Intervention.append(smart_representative)
-                break
-            
+    for mDAG in Observable_mDAGs4.representative_mDAGs_list:
+        if reduces_to_knownQCGap_by_intervention(mDAG):
+            QC_gap_by_Intervention.append(mDAG)
     
     print(len(Observable_mDAGs4.representative_mDAGs_list))
     print(len(QC_gap_by_Intervention))
