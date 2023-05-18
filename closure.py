1135bffc6d0bedf8c8ca9f52eaa783bc082a6222
Elie Wolfe <elie@causarum.org>
2023-05-18 19:20:16 -0400

    BUGFIX: complete transition to new AdjMat class for extracting subgraphs as adjacency matrices.

--- closure.py
+++ closure.py
@@ -8,8 +8,9 @@ elif hexversion >= 0x3060000:
 else:
     cached_property = property
 
-import adjmat_class
+from adjmat_class import AdjMat
 from scipy.sparse.csgraph import connected_components
+
 
 def closure_district_step(core_B, working_B, ds_adjmat, sc_adjmat, return_bidirectedQ=False):
     num_components, component_labels = connected_components(sc_adjmat, directed=False, return_labels=True)
@@ -23,12 +24,12 @@ def closure_district_step(core_B, working_B, ds_adjmat, sc_adjmat, return_bidire
         else:
             return working_B, (num_components==1)
     else:
-        new_ds_adjmat = adjmat_class(ds_adjmat).subadjmat(new_working_B)
+        new_ds_adjmat = AdjMat(ds_adjmat).subadjmat(new_working_B)
         # new_sc_adjmat = adjmat_utils.subadjmat(sc_adjmat, new_working_B)
         return closure_ancestors_step(core_B, new_working_B, new_ds_adjmat, sc_adjmat, num_components, return_bidirectedQ=return_bidirectedQ)
 
 def closure_ancestors_step(core_B, working_B, ds_adjmat, sc_adjmat, num_components, return_bidirectedQ=False):
-    new_working_B = adjmat_class(ds_adjmat).ancestorsplus_of(core_B)
+    new_working_B = AdjMat(ds_adjmat).ancestorsplus_of(core_B)
     assert len(working_B) >= len(new_working_B), "Candidate closure should never grow."
     if len(working_B) == len(new_working_B):
         if not return_bidirectedQ:
@@ -37,7 +38,7 @@ def closure_ancestors_step(core_B, working_B, ds_adjmat, sc_adjmat, num_componen
             return working_B, (num_components==1)
     else:
         # new_ds_adjmat = adjmat_utils.subadjmat(ds_adjmat, new_working_B)
-        new_sc_adjmat = adjmat_class(sc_adjmat).subadjmat(new_working_B)
+        new_sc_adjmat = AdjMat(sc_adjmat).subadjmat(new_working_B)
         return closure_district_step(core_B, new_working_B, ds_adjmat, new_sc_adjmat, return_bidirectedQ=return_bidirectedQ)
 
 
@@ -45,5 +46,6 @@ def closure(core_B, n, ds_adjmat, sc_adjmat, return_bidirectedQ=False):
     return closure_district_step(core_B, np.arange(n+1), ds_adjmat, sc_adjmat, return_bidirectedQ=return_bidirectedQ)
     #Elie: I start with a working set LARGER than the number of visible nodes in order to avoid false termination.
 
+
 def is_this_subadjmat_densely_connected(sc_adjmat, X_indices):
-    return (connected_components(sc_adjmat[np.ix_(X_indices, X_indices)], directed=False, return_labels=False)==1)
+    return (1==connected_components(sc_adjmat[np.ix_(X_indices, X_indices)], directed=False, return_labels=False))
