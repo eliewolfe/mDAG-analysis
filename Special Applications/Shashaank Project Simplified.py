7dc3f3ea48d582a02249776ea90d1dbe6b2a480a
Elie Wolfe <elie@causarum.org>
2023-05-24 15:44:49 -0400

    Set up analysis for Shashaank for N=5

--- Special Applications/Shashaank Project Simplified.py
+++ Special Applications/Shashaank Project Simplified.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 from metagraph_advanced import Observable_unlabelled_mDAGs
 from quantum_mDAG import as_classical_QmDAG
-from the_7_hard_graphs import the_7_hard_graphs
-the_7_hard_ids = set(m.unique_unlabelled_id for m in the_7_hard_graphs)
+# from the_7_hard_graphs import the_7_hard_graphs
+# the_7_hard_ids = set(m.unique_unlabelled_id for m in the_7_hard_graphs)
 
 potentially_interesting_after_HLP = set()
 
@@ -15,16 +15,10 @@ potentially_interesting_classes = unlabelled_mDAGs.difference(metagraph.boring_b
 potentially_interesting_after_HLP = potentially_interesting_classes.intersection(unlabelled_mDAGs)
 print(f"Number of {n}-vis node unlabelled mDAGs not resolved by HLP: {len(potentially_interesting_after_HLP)}")
 print("Conjecture sanity check: ", metagraph.boring_by_virtue_of_Evans.issubset(metagraph.boring_by_virtue_of_HLP))
-# unresolved_by_CI = set([m for m in unlabelled_mDAGs if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by CI: {len(unresolved_by_CI)}")
-# unresolved_by_CI_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_CI)
+print("Does Evans approach coincide with HLP? ", len(metagraph.boring_by_virtue_of_Evans) == len(metagraph.boring_by_virtue_of_HLP))
 unresolved_by_CI_or_HLP = set([m for m in potentially_interesting_after_HLP if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns])
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI: {len(unresolved_by_CI_or_HLP)}")
-# unresolved_by_Shaashank = set([m for m in unlabelled_mDAGs if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by Shaashank's theorem: {len(unresolved_by_Shaashank)}")
-# unresolved_by_Shaashank_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_Shaashank)
 unresolved_by_Shaashank_or_CI_or_HLP = set([m for m in unresolved_by_CI_or_HLP if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs unresolved by Shaashank's theorem or HLP: {len(unresolved_by_Shaashank_or_HLP)}")
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI or Shaashank's theorem: {len(unresolved_by_Shaashank_or_CI_or_HLP)}")
 
 # unresolved_by_esep = set([m for m in unlabelled_mDAGs if m.all_esep_unlabelled in metagraph.latent_free_esep_patterns])
@@ -51,20 +45,13 @@ potentially_interesting_classes = unlabelled_mDAGs.difference(metagraph.boring_b
 potentially_interesting_after_HLP = potentially_interesting_classes.intersection(unlabelled_mDAGs)
 print(f"Number of {n}-vis node unlabelled mDAGs not resolved by HLP: {len(potentially_interesting_after_HLP)}")
 print("Conjecture sanity check: ", metagraph.boring_by_virtue_of_Evans.issubset(metagraph.boring_by_virtue_of_HLP))
-# unresolved_by_CI = set([m for m in unlabelled_mDAGs if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by CI: {len(unresolved_by_CI)}")
-# unresolved_by_CI_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_CI)
+print("Does Evans approach coincide with HLP? ", len(metagraph.boring_by_virtue_of_Evans) == len(metagraph.boring_by_virtue_of_HLP))
 unresolved_by_CI_or_HLP = set(m for m in potentially_interesting_after_HLP if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns)
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI: {len(unresolved_by_CI_or_HLP)}")
-# unresolved_by_Shaashank = set([m for m in unlabelled_mDAGs if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by Shaashank's theorem: {len(unresolved_by_Shaashank)}")
-# unresolved_by_Shaashank_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_Shaashank)
 unresolved_by_Shaashank_or_CI_or_HLP = set([m for m in unresolved_by_CI_or_HLP if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs unresolved by Shaashank's theorem or HLP: {len(unresolved_by_Shaashank_or_HLP)}")
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI or Shaashank's theorem: {len(unresolved_by_Shaashank_or_CI_or_HLP)}")
 unresolved_by_reduction_or_Shaashank_or_CI_or_HLP = set(m for m in unresolved_by_Shaashank_or_CI_or_HLP if believed_interesting_ids.isdisjoint(as_classical_QmDAG(m).unique_unlabelled_ids_obtainable_by_PD_trick))
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI or Shaashank's theorem or subgraph: {len(unresolved_by_reduction_or_Shaashank_or_CI_or_HLP)}")
-print(f"Sanity check, 7 hard graphs still unresolved:", the_7_hard_ids.issubset(m.unique_unlabelled_id for m in unresolved_by_reduction_or_Shaashank_or_CI_or_HLP))
 
 # unresolved_by_esep = set([m for m in unlabelled_mDAGs if m.all_esep_unlabelled in metagraph.latent_free_esep_patterns])
 # print(f"Number of {n}-vis node unlabelled mDAGs not resolved by e-sep: {len(unresolved_by_esep)}")
@@ -90,16 +77,10 @@ potentially_interesting_classes = unlabelled_mDAGs.difference(metagraph.boring_b
 potentially_interesting_after_HLP = potentially_interesting_classes.intersection(unlabelled_mDAGs)
 print(f"Number of {n}-vis node unlabelled mDAGs not resolved by HLP: {len(potentially_interesting_after_HLP)}")
 print("Conjecture sanity check: ", metagraph.boring_by_virtue_of_Evans.issubset(metagraph.boring_by_virtue_of_HLP))
-# unresolved_by_CI = set([m for m in unlabelled_mDAGs if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by CI: {len(unresolved_by_CI)}")
-# unresolved_by_CI_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_CI)
+print("Does Evans approach coincide with HLP? ", len(metagraph.boring_by_virtue_of_Evans) == len(metagraph.boring_by_virtue_of_HLP))
 unresolved_by_CI_or_HLP = set([m for m in potentially_interesting_after_HLP if m.all_CI_unlabelled in metagraph.latent_free_CI_patterns])
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI: {len(unresolved_by_CI_or_HLP)}")
-# unresolved_by_Shaashank = set([m for m in unlabelled_mDAGs if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs not resolved by Shaashank's theorem: {len(unresolved_by_Shaashank)}")
-# unresolved_by_Shaashank_or_HLP = set(potentially_interesting_after_HLP).intersection(unresolved_by_Shaashank)
 unresolved_by_Shaashank_or_CI_or_HLP = set([m for m in unresolved_by_CI_or_HLP if not m.interesting_via_e_sep_theorem])
-# print(f"Number of {n}-vis node unlabelled mDAGs unresolved by Shaashank's theorem or HLP: {len(unresolved_by_Shaashank_or_HLP)}")
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI or Shaashank's theorem: {len(unresolved_by_Shaashank_or_CI_or_HLP)}")
 unresolved_by_reduction_or_Shaashank_or_CI_or_HLP = set(m for m in unresolved_by_Shaashank_or_CI_or_HLP if believed_interesting_ids.isdisjoint(as_classical_QmDAG(m).unique_unlabelled_ids_obtainable_by_PD_trick))
 print(f"Number of {n}-vis node unlabelled mDAGs unresolved by HLP or CI or Shaashank's theorem or subgraph: {len(unresolved_by_reduction_or_Shaashank_or_CI_or_HLP)}")
